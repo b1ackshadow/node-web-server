b125d3a1e26e02aa4102dc2302da6e5bf93ccd93
blackshadow <b14cksh4d0w369@gmail.com>
2018-04-26 20:11:02 +0530

    Setup for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','hbs');
@@ -27,6 +28,6 @@ app.get('/about',(req,res)=>{
     pageTitle:'About page',
   });
 });
-app.listen(3000,()=>{
-  console.log('Server is up on 3000');
+app.listen(port,()=>{
+  console.log('Server is up on '+port);
 });
